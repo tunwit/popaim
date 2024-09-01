977cd554b5de4e7a2e683d132d97f6e2b76d55c7
tunwit <tunwit2458@gmail.com>
2024-09-01 23:03:00 +0700

    Fix bug on preventdefault

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
   const [count,setCount] = useState(1)
   const [play] = useSound("/pop.mp3")
   const downhandler = (e:SyntheticEvent) =>{
-    e.preventDefault();
     setAim(true)
     setCount(count+1)
     play()
@@ -22,8 +21,8 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="fixed h-full w-full right-56 sm:right-0"   onTouchEnd={(e:SyntheticEvent)=>{uphandler(e)}}  onTouchStart={(e:SyntheticEvent)=>{downhandler(e)}} onMouseUp={(e:SyntheticEvent)=>{uphandler(e)}} onMouseDown={(e:SyntheticEvent)=>{downhandler(e)}}>
-        <Image alt="open" className="min-w-[1500px]" src={aim_open} fill quality={100}/>
-        <Image alt="close" className="min-w-[1500px]" hidden={aim} src={aim_close} fill quality={100}/>
+        <Image alt="open" placeholder="blur" className="min-w-[1500px]" src={aim_open} fill quality={100}/>
+        <Image alt="close" placeholder="blur" className="min-w-[1500px]" hidden={aim} src={aim_close} fill quality={100}/>
       </div>
       <div className="absolute top-10">
         <p className="text-white font-bold text-4xl">POP! {count}</p>

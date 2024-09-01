38e3c5a8356c6de87aef977330dd92bc69962bdb
tunwit <tunwit2458@gmail.com>
2024-09-01 22:58:53 +0700

    fix image not align properly

--- app/page.tsx
+++ app/page.tsx
@@ -21,9 +21,9 @@ export default function Home() {
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className="fixed h-full w-full right-10"   onTouchEnd={(e:SyntheticEvent)=>{uphandler(e)}}  onTouchStart={(e:SyntheticEvent)=>{downhandler(e)}} onMouseUp={(e:SyntheticEvent)=>{uphandler(e)}} onMouseDown={(e:SyntheticEvent)=>{downhandler(e)}}>
-        <Image alt="open" className="min-w-[800px]" src={aim_open} fill quality={100}/>
-        <Image alt="close" className="min-w-[800px]" hidden={aim} src={aim_close} fill quality={100}/>
+      <div className="fixed h-full w-full right-56 sm:right-0"   onTouchEnd={(e:SyntheticEvent)=>{uphandler(e)}}  onTouchStart={(e:SyntheticEvent)=>{downhandler(e)}} onMouseUp={(e:SyntheticEvent)=>{uphandler(e)}} onMouseDown={(e:SyntheticEvent)=>{downhandler(e)}}>
+        <Image alt="open" className="min-w-[1500px]" src={aim_open} fill quality={100}/>
+        <Image alt="close" className="min-w-[1500px]" hidden={aim} src={aim_close} fill quality={100}/>
       </div>
       <div className="absolute top-10">
         <p className="text-white font-bold text-4xl">POP! {count}</p>
